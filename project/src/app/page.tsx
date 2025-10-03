8cb38f8415e269e82faa73c9ab2463d83c29dc1a
Davey Munters <davey@weseedo.nl>
2025-10-03 14:51:14 +0200

    Changed structure of docker container and readme

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>test</h1>
+    <div className="min-h-screen flex items-center justify-center bg-[#363535]">
+      <h1 className="text-3xl font-bold text-[#f1f1f1]">test</h1>
     </div>
   );
 }
